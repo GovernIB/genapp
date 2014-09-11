29502917ee24b84b1dd0bd5986f7712f396875a1
anadal-ibit <anadal-ibit@8acd16dd-9d0b-4b63-88f5-00443dff9d07>
2014-09-11 09:42:23 +0000

    Adaptant a repositori maven de sourceforge

--- genapp-core/branches/genapp-1.0/gen/src/main/resources/genapp/baseDirs/lib/install_genapp_core_jar.bat
+++
@@ -1,3 +0,0 @@
-
-
-mvn install:install-file -Dfile=./genapp-core-${genapp_version}.jar -DgroupId=org.fundaciobit.genapp -DartifactId=genapp-core -Dversion=${genapp_version} -Dpackaging=jar
--- genapp-core/branches/genapp-1.0/gen/src/main/resources/genapp/baseDirs/lib/install_genapp_web_jar.bat
+++
@@ -1,4 +0,0 @@
-
-mvn install:install-file -Dfile=./genapp-web-${genapp_version}.jar -DgroupId=org.fundaciobit.genapp -DartifactId=genapp-web -Dversion=${genapp_version} -Dpackaging=jar
-
-
