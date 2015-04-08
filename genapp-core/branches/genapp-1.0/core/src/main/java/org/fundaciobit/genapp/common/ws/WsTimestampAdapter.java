8b383be82330283b2327d95cc6dd77024ff0b703
anadal-ibit <anadal-ibit@8acd16dd-9d0b-4b63-88f5-00443dff9d07>
2015-04-08 09:15:11 +0000

    Mètodes per API de WS: Time, Date, Timestamp

--- genapp-core/branches/genapp-1.0/core/src/main/java/org/fundaciobit/genapp/common/ws/WsTimestampAdapter.java
+++ genapp-core/branches/genapp-1.0/core/src/main/java/org/fundaciobit/genapp/common/ws/WsTimestampAdapter.java
@@ -1,8 +1,10 @@
 package org.fundaciobit.genapp.common.ws;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.sql.Timestamp;
 
+import javax.xml.bind.DatatypeConverter;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 /**
@@ -21,4 +23,28 @@ public class WsTimestampAdapter extends XmlAdapter<Date, Timestamp> {
   public Timestamp unmarshal(Date v) {
     return new Timestamp(v.getTime());
   }
+  
+  
+  
+  public static Timestamp parseDateTime(String s) {
+    if (s == null) {
+      return null;
+    }
+    return new Timestamp(DatatypeConverter.parseDateTime(s).getTime().getTime());
+  
+  }
+  
+  public static String printDateTime(Timestamp ts) {
+  
+    if (ts == null) {
+      return null;
+    }    
+
+    Calendar c = Calendar.getInstance();
+    c.setTimeInMillis(ts.getTime());
+    return DatatypeConverter.printDateTime(c);
+  
+  }
+  
+  
 }
