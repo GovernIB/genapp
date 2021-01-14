f7cbd311fd1f7a5923598fb1cf9c704f1cdaa011
anadal-fundaciobit <anadal@fundaciobit.org>
2021-01-14 11:19:54 +0100

    https://github.com/GovernIB/carpeta/issues/113

--- genapp-core/src/main/java/org/fundaciobit/genapp/common/validation/AbstractValidator.java
+++ genapp-core/src/main/java/org/fundaciobit/genapp/common/validation/AbstractValidator.java
@@ -3,62 +3,10 @@ package org.fundaciobit.genapp.common.validation;
 
 
 /**
- * XYZ ZZZ   Esborrar
+ *
  * @author anadal
  *
  */
 public abstract class AbstractValidator {
 
-	/* XYZ ZZZ Esborrar
-	public String toString(Object obj) {
-		return (String)obj;
-    }
-	
-	public Boolean toBoolean(Object obj) {
-		return Boolean.parseBoolean((String)obj);
-	}
-	
-	public Integer toInteger(Object obj) {
-		return Integer.parseInt((String)obj);
-	}
-	
-	public Short toShort(Object obj) {
-		return Short.parseShort((String)obj);
-	}
-	
-	public Byte toByte(Object obj) {
-		return Byte.parseByte((String)obj);
-	}
-	
-	
-	public Long toLong(Object obj) {
-		return Long.parseLong((String)obj);
-	}
-	
-	public BigDecimal toBigDecimal(Object obj) {
-		return BigDecimal.parseBigDecimal((String)obj);
-	}
-
-	
-	public Float toFloat(Object obj) {
-		return Float.parseFloat((String)obj);
-	}
-	
-	public Double toDouble(Object obj) {
-		return Double.parseDouble((String)obj);
-	}
-	
- 
-	public Date toDate(Object obj) {
-		Integer.parseInt((String)obj);
-	}
-	
-	public Time toTime(Object obj) {
-		Integer.parseInt((String)obj);
-	}
-	
-	public Timestamp toTimestamp(Object obj) {
-		Integer.parseInt((String)obj);
-	}
-*/
 }
