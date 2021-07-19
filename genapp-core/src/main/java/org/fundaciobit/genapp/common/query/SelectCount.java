649fec9205f6d8b32efc885dd69a1987d2dcf8b3
Antoni Nadal <anadal@fundaciobit.org>
2021-07-19 11:04:44 +0200

    Tutorial GenAppSQL (GenQL) #53

--- genapp-core/src/main/java/org/fundaciobit/genapp/common/query/SelectCount.java
+++ genapp-core/src/main/java/org/fundaciobit/genapp/common/query/SelectCount.java
@@ -5,7 +5,6 @@ import java.math.BigInteger;
 
 import org.fundaciobit.genapp.common.i18n.I18NException;
 
-
 /**
  * 
  * @author anadal
@@ -13,38 +12,53 @@ import org.fundaciobit.genapp.common.i18n.I18NException;
  */
 public class SelectCount extends Select<Long> {
 
-  @Override
-  public String getSelectString() {
-    return "count(*)";
-  }
+    final String countParam;
+    
+    public SelectCount() {
+        this.countParam = "*";
+    }
+    
+    
+    public SelectCount(Field<?> field) {
+        this.countParam = field.fullName;
+    }
+    
+    
+    public SelectCount(Select<?> select) {
+        this.countParam = select.getSelectString().replace('(', ' ').replace(')', ' ');
+    }
+    
+    
+    
+    
+    @Override
+    public String getSelectString() {
+        return "count(" + countParam+ ")";
+    }
 
-  @Override
-  public Long getFromObject(Object obj) throws I18NException {
-    return objectToLong(obj);
-  }
-  
-  
-  public static Long objectToLong(Object obj)  throws I18NException {
-    if (obj == null) {
-      return null;
+    @Override
+    public Long getFromObject(Object obj) throws I18NException {
+        return objectToLong(obj);
     }
-    if (obj instanceof BigDecimal) {
-      BigDecimal bigdec = (BigDecimal)obj;
-      return bigdec.longValue();
+
+    public static Long objectToLong(Object obj) throws I18NException {
+        if (obj == null) {
+            return null;
+        }
+        if (obj instanceof BigDecimal) {
+            BigDecimal bigdec = (BigDecimal) obj;
+            return bigdec.longValue();
+        }
+        if (obj instanceof BigInteger) {
+            BigInteger bigint = (BigInteger) obj;
+            return bigint.longValue();
+        }
+        if (obj instanceof Number) {
+            Number number = (Number) obj;
+            return number.longValue();
+        }
+        System.err.println("Valor desconegut retornat per la funcio count(??): " + obj.getClass() + " [ " + obj + " ]");
+        throw new I18NException("genapp.unknowtype", String.valueOf(obj.getClass().getName()), String.valueOf(obj));
     }
-    if (obj instanceof BigInteger) {
-      BigInteger bigint = (BigInteger)obj;
-      return bigint.longValue();
-    }
-    if (obj instanceof Number) {
-      Number number = (Number)obj;
-      return number.longValue();
-    }
-    System.err.println("Valor desconegut retornat per la funcion count(*): "
-         + obj.getClass() + " [ " + obj+ " ]");
-    throw new I18NException("genapp.unknowtype",
-        String.valueOf(obj.getClass().getName()), String.valueOf(obj));
-  }
 
 }
-
