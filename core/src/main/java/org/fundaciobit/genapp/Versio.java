10afc516aba2786d8c7ff98aa6768a95455a759e
Antoni Reus <areus@fundaciobit.org>
2021-08-09 10:13:27 +0200

    fixar versió 1.1-SNAPSHOT

--- core/src/main/java/org/fundaciobit/genapp/Versio.java
+++ core/src/main/java/org/fundaciobit/genapp/Versio.java
@@ -3,5 +3,5 @@ package org.fundaciobit.genapp;
 * Codi autogenerat a partir del fitxer Versio.java.template.
 */
 public interface Versio {
-public static final String VERSIO="1.0.3";
+public static final String VERSIO="1.1-SNAPSHOT";
 }
