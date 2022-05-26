650b10ee262a1bb02112d469bd1dc16fe0c9c259
pautrias <ptrias@fundaciobit.org>
2022-05-26 10:42:33 +0200

    Utilizar variables containsRoleUser i containsRoleAdmin de la classe AuthenticationSuccessListener per eliminar Warnings #111

--- genapp-generator/src/main/resources/genapp/back/src/main/java/x_package_x/back/security/AuthenticationSuccessListener.java
+++ genapp-generator/src/main/resources/genapp/back/src/main/java/x_package_x/back/security/AuthenticationSuccessListener.java
@@ -26,74 +26,64 @@ import ${package}.commons.utils.Constants;
  * 
  */
 @Component
-public class AuthenticationSuccessListener implements
-    ApplicationListener<InteractiveAuthenticationSuccessEvent> {
+public class AuthenticationSuccessListener implements ApplicationListener<InteractiveAuthenticationSuccessEvent> {
 
-  protected final Logger log = Logger.getLogger(getClass());
-   
+    protected final Logger log = Logger.getLogger(getClass());
 
-  @Override
-  public synchronized void onApplicationEvent(InteractiveAuthenticationSuccessEvent event) {
-
-    SecurityContext sc = SecurityContextHolder.getContext();
-    Authentication au = sc.getAuthentication();
-    
-    if (au == null) {
-      // TODO traduccio
-      throw new LoginException("NO PUC ACCEDIR A LA INFORMACIO de AUTENTICACIO");
-    }
-
-    User user = (User) au.getPrincipal();
-    
-    String name = user.getUsername();
-    log.info(" =================================================================");
-    log.info(" ============ Login Usuari: " + name);
-
-    // Cercam si té el ROLE_USER o ROLE_ADMIN
-    Collection<GrantedAuthority> realAuthorities = user.getAuthorities();
-    boolean containsRoleUser = false;
-    boolean containsRoleAdmin = false;
-    for (GrantedAuthority grantedAuthority : realAuthorities) {
-      String rol = grantedAuthority.getAuthority();
-      log.info("Rol REAL : " + rol);
-      if (Constants.ROLE_USER.equals(rol)) {
-        containsRoleUser = true;
-      }
-      if (Constants.ROLE_ADMIN.equals(rol)) {
-        containsRoleAdmin = true;
-      }
-    }
-
-    if(log.isDebugEnabled()) {
-    	log.debug("containsRoleUser: " + containsRoleUser);
-    	log.debug("containsRoleAdmin: " + containsRoleAdmin);
-    }
-
-    // TODO GENAPP Obtenir idioma de l'usuari. Null = idioma per defecte.
-    String language = null;
-    
-    // TODO GenApp Afegir el codi oportu despres del login
-
-    LoginInfo loginInfo;
-    // create a new authentication token
-    loginInfo = new LoginInfo(user, name, new HashSet<GrantedAuthority>(realAuthorities), language);
-
-    // and set the authentication of the current Session context
-    SecurityContextHolder.getContext().setAuthentication(loginInfo.generateToken());
-    
-    
-    log.info(">>>>>> Final del Process d'autenticació.");
-    log.info(" =================================================================");
-
-  }
-  
-  
-  
-  public static final Comparator<GrantedAuthority> GRANTEDAUTHORITYCOMPARATOR = new Comparator<GrantedAuthority>() {
     @Override
-    public int compare(GrantedAuthority o1, GrantedAuthority o2) {
-      return -o1.getAuthority().compareTo(o2.getAuthority());
+    public synchronized void onApplicationEvent(InteractiveAuthenticationSuccessEvent event) {
+
+        SecurityContext sc = SecurityContextHolder.getContext();
+        Authentication au = sc.getAuthentication();
+
+        if (au == null) {
+            // TODO traduccio
+            throw new LoginException("NO PUC ACCEDIR A LA INFORMACIO de AUTENTICACIO");
+        }
+
+        User user = (User) au.getPrincipal();
+
+        String name = user.getUsername();
+        log.info(" =================================================================");
+        log.info(" ============ Login Usuari: " + name);
+
+        // Cercam si té el ROLE_USER o ROLE_ADMIN
+        Collection<GrantedAuthority> realAuthorities = user.getAuthorities();
+        boolean containsRoleUser = false;
+        boolean containsRoleAdmin = false;
+        for (GrantedAuthority grantedAuthority : realAuthorities) {
+            String rol = grantedAuthority.getAuthority();
+            log.info("Rol REAL : " + rol);
+            if (Constants.ROLE_USER.equals(rol)) {
+                containsRoleUser = true;
+            }
+            if (Constants.ROLE_ADMIN.equals(rol)) {
+                containsRoleAdmin = true;
+            }
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("containsRoleUser: " + containsRoleUser);
+            log.debug("containsRoleAdmin: " + containsRoleAdmin);
+        }
+
+        // TODO GENAPP Obtenir idioma de l'usuari. Null = idioma per defecte.
+        String language = null;
+
+        // TODO GenApp Afegir el codi oportu despres del login
+
+        LoginInfo loginInfo;
+        // create a new authentication token
+        loginInfo = new LoginInfo(user, name, new HashSet<GrantedAuthority>(realAuthorities), language);
+
+        // and set the authentication of the current Session context
+        SecurityContextHolder.getContext().setAuthentication(loginInfo.generateToken());
+
+        log.info(">>>>>> Final del Process d'autenticació.");
+        log.info(" =================================================================");
+
     }
-  };
-  
+
+    public static final Comparator<GrantedAuthority> GRANTEDAUTHORITYCOMPARATOR=new Comparator<GrantedAuthority>(){@Override public int compare(GrantedAuthority o1,GrantedAuthority o2){return-o1.getAuthority().compareTo(o2.getAuthority());}};
+
 }
