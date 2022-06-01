e5b08ed9d1f6fde8eac6395d9a9b7591a1d37e76
fbosch-fundaciobit <fbosch@fundaciobit.org>
2022-06-01 11:25:11 +0200

    Afegir codi java per consultar informació de l'usuari loguejat a traves del Plugin de User Information #101

--- genapp-generator/src/main/resources/genapp/back/src/main/java/x_package_x/back/security/LoginInfo.java
+++ genapp-generator/src/main/resources/genapp/back/src/main/java/x_package_x/back/security/LoginInfo.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
+import org.fundaciobit.pluginsib.userinformation.UserInfo;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 
@@ -18,45 +19,51 @@ import org.springframework.security.core.userdetails.User;
  */
 public class LoginInfo {
 
-	final User springSecurityUser;
+    final User springSecurityUser;
 
-	Set<String> roles;
+    Set<String> roles;
 
-	Set<GrantedAuthority> grantedAuthorities;
+    Set<GrantedAuthority> grantedAuthorities;
 
-	final String username;
+    final String username;
 
-	final String language;
+    final String language;
 
-	// TODO Add your fields HERE
+    final UserInfo userInfo;
 
-	/**
-	 * @param usuari
-	 * @param entitatActual
-	 * @param roles
-	 */
-	public LoginInfo(User springSecurityUser, String username, Set<GrantedAuthority> grantedAuthorities,
-			String language) {
-		this.springSecurityUser = springSecurityUser;
-		this.username = username;
-		this.language = language;
+    // TODO Add your fields HERE
 
-		this.grantedAuthorities = grantedAuthorities;
-		this.roles = new HashSet<String>();
+    /**
+     * @param usuari
+     * @param entitatActual
+     * @param roles
+     */
+    public LoginInfo(User springSecurityUser, String username, Set<GrantedAuthority> grantedAuthorities,
+            String language, UserInfo userInfo) {
+        this.springSecurityUser = springSecurityUser;
+        this.username = username;
+        this.language = language;
+        this.userInfo = userInfo;
+        this.grantedAuthorities = grantedAuthorities;
+        this.roles = new HashSet<String>();
 
-		for (GrantedAuthority grantedAuthority : this.grantedAuthorities) {
-			this.roles.add(grantedAuthority.getAuthority());
-		}
+        for (GrantedAuthority grantedAuthority : this.grantedAuthorities) {
+            this.roles.add(grantedAuthority.getAuthority());
+        }
 
-	}
+    }
 
-	public Set<String> getRoles() {
-		return this.roles;
-	}
+    public Set<String> getRoles() {
+        return this.roles;
+    }
 
-	public Set<GrantedAuthority> getGrantedAuthorities() {
-		return grantedAuthorities;
-	}
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public Set<GrantedAuthority> getGrantedAuthorities() {
+        return grantedAuthorities;
+    }
 
     public static boolean hasRole(String role) {
         try {
@@ -66,43 +73,43 @@ public class LoginInfo {
         }
     }
 
-	public String getUsername() {
-		return this.username;
-	}
+    public String getUsername() {
+        return this.username;
+    }
 
-	public String getLanguage() {
-		return this.language;
-	}
+    public String getLanguage() {
+        return this.language;
+    }
 
-	public UsernamePasswordAuthenticationToken generateToken() {
-		UsernamePasswordAuthenticationToken authToken;
-		Set<GrantedAuthority> roles = getGrantedAuthorities();
-		authToken = new UsernamePasswordAuthenticationToken(this.springSecurityUser, "", roles);
-		authToken.setDetails(this);
-		return authToken;
-	}
+    public UsernamePasswordAuthenticationToken generateToken() {
+        UsernamePasswordAuthenticationToken authToken;
+        Set<GrantedAuthority> roles = getGrantedAuthorities();
+        authToken = new UsernamePasswordAuthenticationToken(this.springSecurityUser, "", roles);
+        authToken.setDetails(this);
+        return authToken;
+    }
 
-	public static LoginInfo getInstance() throws LoginException {
-		Object obj;
-		try {
-			obj = SecurityContextHolder.getContext().getAuthentication().getDetails();
-		} catch (Exception e) {
-			// TODO traduccio
-			throw new LoginException("Error intentant obtenir informació de Login.", e);
-		}
+    public static LoginInfo getInstance() throws LoginException {
+        Object obj;
+        try {
+            obj = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        } catch (Exception e) {
+            // TODO traduccio
+            throw new LoginException("Error intentant obtenir informació de Login.", e);
+        }
 
-		if (obj == null) {
-			// TODO traduccio
-			throw new LoginException("La informació de Login és buida");
-		}
+        if (obj == null) {
+            // TODO traduccio
+            throw new LoginException("La informació de Login és buida");
+        }
 
-		if (obj instanceof LoginInfo) {
-			return (LoginInfo) obj;
-		} else {
-			// TODO traduccio
-			throw new LoginException("La informació de Login no és del tipus esperat." + " Hauria de ser de tipus "
-					+ LoginInfo.class.getName() + " i és del tipus " + obj.getClass().getName());
-		}
-	}
+        if (obj instanceof LoginInfo) {
+            return (LoginInfo) obj;
+        } else {
+            // TODO traduccio
+            throw new LoginException("La informació de Login no és del tipus esperat." + " Hauria de ser de tipus "
+                    + LoginInfo.class.getName() + " i és del tipus " + obj.getClass().getName());
+        }
+    }
 
 }
