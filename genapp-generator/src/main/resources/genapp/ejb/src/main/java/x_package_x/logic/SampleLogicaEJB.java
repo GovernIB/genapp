74078932d55ea779298a6a5d06da396725ea2216
pautrias <ptrias@fundaciobit.org>
2022-08-03 12:29:31 +0200

    DeleteFull que esborra Fitxers quan es fa RollBack #58

--- genapp-generator/src/main/resources/genapp/ejb/src/main/java/x_package_x/logic/SampleLogicaEJB.java
+++ genapp-generator/src/main/resources/genapp/ejb/src/main/java/x_package_x/logic/SampleLogicaEJB.java
@@ -1,5 +1,10 @@
 package ${package}.logic;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.annotation.Resource;
+import javax.ejb.EJB;
 
 /*
 import ${package}.ejb.AnnexEJB;
@@ -15,6 +20,7 @@ import org.fundaciobit.genapp.common.i18n.I18NException;
 
 //import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.transaction.TransactionSynchronizationRegistry;
 
 /**
  * 
@@ -24,45 +30,84 @@ import javax.ejb.Stateless;
 @Stateless(name = "SampleLogicaEJB")
 public class SampleLogicaEJB implements SampleLogicaService {
 
+//    @EJB(mappedName=${package}.ejb.FitxerService.JNDI_NAME)protected ${package}.
+//    ejb.FitxerService fitxerEjb;
+//
+//    @Resource(mappedName = "java:comp/TransactionSynchronizationRegistry")
+//    protected TransactionSynchronizationRegistry transactionSynchronizationRegistry;
+//
+//    @Override
+//    public void deleteFull(AnnexJPA annex) throws I18NException {
+//        log.info("Comença deleteFull");
+//
+//        // Borram Annex a BD
+//        delete(annex);
+//
+//        Long[] fitxers = new Long[] { annex.getFitxer1ID(), annex.getFitxer2ID() };
+//
+//        Set<Long> filesToDelete = new HashSet<Long>();
+//
+//        // Borram fitxers a BD
+//        for (Long f : fitxers) {
+//            if (f != null) {
+//                fitxerEjb.delete(f);
+//                filesToDelete.add(f);
+//            }
+//        }
+//
+//        tsRegistry.registerInterposedSynchronization(new PreCommitFiles(filesToDelete));
+//
+//        log.info("Final deleteFull");
+//    }
 
-}
- 
- /**
-@Stateless(name = "AnnexLogicaEJB")
-public class AnnexLogicaEJB extends AnnexEJB implements AnnexLogicaService,
-    AnnexFields {
+};
 
+//public class PreCommitFiles implements Synchronization {
+//    public final Set<Long> files;
+//
+//    public PreCommitFiles(Set<Long> filesToDelete) {
+//        this.files = filesToDelete;
+//    }
+//
+//    @Override
+//    public void beforeCompletion() {
+//    }
+//
+//    @Override
+//    public void afterCompletion(int status) {
+//         if ( status == Status.STATUS_COMMITTED) {
+//                  FileSystemManager.eliminarArxius(files);
+//         }
+//    }
+//};
 
-  @EJB(mappedName = FitxerService.JNDI_NAME)
-  private FitxerService fitxerEjb;
-
-  @EJB(mappedName = ${package}.ejb.AnnexFirmatService.JNDI_NAME)
-  protected ${package}.ejb.AnnexFirmatServiceannexFirmatEjb;
-  
-  
-  @Override
-  public AnnexJPA createFull(AnnexJPA annex) throws I18NException {
-    // TODO Validar !!!
-    
-    return (AnnexJPA)create(annex);
-  }
-  
-
-
-  @Override
-  public Set<Long> deleteFull(AnnexJPA annex) throws I18NException {
-    
-    Set<Long> files = new HashSet<Long>();
-    
-    if (annex == null) {
-      return files;
-    }
-    
-    // Annex
-    delete(annex);
-    
-    return files;
-  }
-
-}
-*/
+/**
+ * @Stateless(name = "AnnexLogicaEJB") public class AnnexLogicaEJB extends
+ *                 AnnexEJB implements AnnexLogicaService, AnnexFields {
+ * 
+ * 
+ * @EJB(mappedName = FitxerService.JNDI_NAME) private FitxerService fitxerEjb;
+ * 
+ * @EJB(mappedName = ${package}.ejb.AnnexFirmatService.JNDI_NAME) protected
+ *                 ${package}.ejb.AnnexFirmatServiceannexFirmatEjb;
+ * 
+ * 
+ * @Override public AnnexJPA createFull(AnnexJPA annex) throws I18NException {
+ *           // TODO Validar !!!
+ * 
+ *           return (AnnexJPA)create(annex); }
+ * 
+ * 
+ * 
+ * @Override public Set<Long> deleteFull(AnnexJPA annex) throws I18NException {
+ * 
+ *           Set<Long> files = new HashSet<Long>();
+ * 
+ *           if (annex == null) { return files; }
+ * 
+ *           // Annex delete(annex);
+ * 
+ *           return files; }
+ * 
+ *           }
+ */
--- genapp-generator/src/main/resources/genapp/ejb/src/main/java/x_package_x/logic/SampleLogicaService.java
+++ genapp-generator/src/main/resources/genapp/ejb/src/main/java/x_package_x/logic/SampleLogicaService.java
@@ -3,6 +3,7 @@ package ${package}.logic;
 //import java.util.Set;
 
 import javax.ejb.Local;
+
 /*
 import ${package}.ejb.AnnexService;
 import ${package}.persistence.AnnexJPA;
@@ -16,13 +17,12 @@ import org.fundaciobit.genapp.common.i18n.I18NException;
  */
 @Local
 public interface SampleLogicaService /* extends AnnexService */ {
-	
-	public static final String JNDI_NAME = "java:app/${name}-ejb/SampleLogicaEJB!${package}.logic.SampleLogicaService";
 
+    public static final String JNDI_NAME = "java:app/${name}-ejb/SampleLogicaEJB!${package}.logic.SampleLogicaService";
 /*
-  public Set<Long> deleteFull(AnnexJPA annex) throws I18NException;
-  
-  public AnnexJPA createFull(AnnexJPA annex) throws I18NException;
-  */
-}
+    public void deleteFull(AnnexJPA annex) throws I18NException;
 
+    
+     * public AnnexJPA createFull(AnnexJPA annex) throws I18NException;
+     */
+}
