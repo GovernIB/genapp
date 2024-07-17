eb74010c23aa98f0b87f08a229df6bc25f342166
Antoni Nadal <anadal@fundaciobit.org>
2024-07-17 14:05:43 +0200

    Millorar ApiExterna i ApiInterna #136

---
+++ demo/generat/demogenapp-api-externa-client-exemplepublic-v1/docs/VersioApi.md
@@ -0,0 +1,74 @@
+# VersioApi
+
+All URIs are relative to */demogenappapi/externa*
+
+| Method | HTTP request | Description |
+|------------- | ------------- | -------------|
+| [**versio**](VersioApi.md#versio) | **GET** /public/exemplepublic/versio | Versio de l&#39;Aplicació |
+
+
+
+## versio
+
+> ExamplePojo versio(idioma)
+
+Versio de l&#39;Aplicació
+
+### Example
+
+```java
+// Import classes:
+import org.fundaciobit.demogenapp.apiexterna.client.exemplepublic.v1.services.ApiClient;
+import org.fundaciobit.demogenapp.apiexterna.client.exemplepublic.v1.services.ApiException;
+import org.fundaciobit.demogenapp.apiexterna.client.exemplepublic.v1.services.Configuration;
+import org.fundaciobit.demogenapp.apiexterna.client.exemplepublic.v1.services.models.*;
+import org.fundaciobit.demogenapp.apiexterna.client.exemplepublic.v1.api.VersioApi;
+
+public class Example {
+    public static void main(String[] args) {
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath("/demogenappapi/externa");
+
+        VersioApi apiInstance = new VersioApi(defaultClient);
+        String idioma = "ca"; // String | Codi de l'idioma
+        try {
+            ExamplePojo result = apiInstance.versio(idioma);
+            System.out.println(result);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling VersioApi#versio");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
+    }
+}
+```
+
+### Parameters
+
+
+| Name | Type | Description  | Notes |
+|------------- | ------------- | ------------- | -------------|
+| **idioma** | **String**| Codi de l&#39;idioma | [optional] |
+
+### Return type
+
+[**ExamplePojo**](ExamplePojo.md)
+
+### Authorization
+
+No authorization required
+
+### HTTP request headers
+
+- **Content-Type**: Not defined
+- **Accept**: application/json
+
+
+### HTTP response details
+| Status code | Description | Response headers |
+|-------------|-------------|------------------|
+| **404** | Paràmetres incorrectes |  -  |
+| **200** | Versió i idioma |  -  |
+
---
+++ demo/generat/demogenapp-api-externa-client-exemplesecure-v1/src/main/java/org/fundaciobit/demogenapp/apiexterna/client/exemplesecure/v1/api/ExempleSeguritzatApi.java
@@ -0,0 +1,76 @@
+package org.fundaciobit.demogenapp.apiexterna.client.exemplesecure.v1.api;
+
+import org.fundaciobit.demogenapp.apiexterna.client.exemplesecure.v1.services.ApiException;
+import org.fundaciobit.demogenapp.apiexterna.client.exemplesecure.v1.services.ApiClient;
+import org.fundaciobit.demogenapp.apiexterna.client.exemplesecure.v1.services.Configuration;
+import org.fundaciobit.demogenapp.apiexterna.client.exemplesecure.v1.services.Pair;
+
+import javax.ws.rs.core.GenericType;
+
+import org.fundaciobit.demogenapp.apiexterna.client.exemplesecure.v1.model.ConstantsWs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
+public class ExempleSeguritzatApi {
+  private ApiClient apiClient;
+
+  public ExempleSeguritzatApi() {
+    this(Configuration.getDefaultApiClient());
+  }
+
+  public ExempleSeguritzatApi(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  public ApiClient getApiClient() {
+    return apiClient;
+  }
+
+  public void setApiClient(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  /**
+   * Fa un ECHO
+   * 
+   * @param echoInput Cadena a retornar (optional)
+   * @return a {@code String}
+   * @throws ApiException if fails to make API call
+   */
+  public String echo(String echoInput) throws ApiException {
+    Object localVarPostBody = null;
+    
+    // create path and map variables
+    String localVarPath = "/secure/exempleseguritzat/echo".replaceAll("\\{format\\}","json");
+
+    // query params
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, String> localVarCookieParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+    localVarQueryParams.addAll(apiClient.parameterToPairs("", "echoInput", echoInput));
+
+    
+    
+    
+    final String[] localVarAccepts = {
+      "application/json"
+    };
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {
+      
+    };
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] { "BasicAuth" };
+
+    GenericType<String> localVarReturnType = new GenericType<String>() {};
+    return apiClient.invokeAPI(localVarPath, "GET", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarCookieParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+      }
+}
