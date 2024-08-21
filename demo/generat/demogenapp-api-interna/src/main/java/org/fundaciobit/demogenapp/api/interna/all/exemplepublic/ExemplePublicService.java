680af678b124223cee191d1417ff48eb85667080
Antoni Nadal <anadal@fundaciobit.org>
2024-08-21 12:03:12 +0200

    Substitució de jars per mòduls JBOss a ApiInterna i ApiExterna #162

--- demo/generat/demogenapp-api-interna/src/main/java/org/fundaciobit/demogenapp/api/interna/all/exemplepublic/ExemplePublicService.java
+++ demo/generat/demogenapp-api-interna/src/main/java/org/fundaciobit/demogenapp/api/interna/all/exemplepublic/ExemplePublicService.java
@@ -85,11 +85,10 @@ public class ExemplePublicService {
             @Parameter(
                     description = "Codi de l'idioma",
                     required = false,
-                    example = "ca",
                     examples = { @io.swagger.v3.oas.annotations.media.ExampleObject(name = "Català", value = "ca"),
                                  @io.swagger.v3.oas.annotations.media.ExampleObject(name = "Castellano", value = "es")},
                     schema = @Schema(implementation = String.class)) 
-                          @Pattern(regexp = "^ca|es$")   @QueryParam("idioma") String idioma) {
+                                @Pattern(regexp = "^ca|es$") @QueryParam("idioma") String idioma) {
 
         try {
             ExemplePojo exemple = new ExemplePojo(new Version().getVersion() + "_" + idioma);
