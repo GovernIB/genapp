d086eceead45df4483c5b8c8cea8fe26a66585f0
anadal-fundaciobit <anadal@fundaciobit.org>
2026-08-07 12:30:15 +0200

    Excloure dels ears el mòdul org.jboss.resteasy.resteasy-json-binding-provider #156

--- demo/netejar.bat
+++ demo/netejar.bat
@@ -1,6 +1,13 @@
 
-if exist "generat" (
-    rd /S /Q "generat"
+@echo off
+set "GENERAT_DIR=%CD%\generat"
+
+REM Esborrar tots els subdirectoris excepte "doc"
+for /D %%D in ("%GENERAT_DIR%\*") do (
+    if /I not "%%~nxD"=="doc" (
+        rd /S /Q "%%D"
+    )
 )
 
-mkdir "generat"
+REM Esborrar tots els fitxers de l'arrel de "generat"
+del /Q "%GENERAT_DIR%\*" >nul 2>&1
